905700f154feb7405b9b76193d1f1324c3113b83
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-05-22 12:23:56 -0400

    Added the icon

--- Develop/app.js
+++ Develop/app.js
@@ -5,6 +5,7 @@ var temp = document.querySelector('.temp');
 var humi = document.querySelector('.humi');
 var wind = document.querySelector('.wind');
 var uvIndex = document.querySelector('#uvIndex');
+var weatherIcon = document.querySelector('.weatherIcon');
 // Create a function to reuse
 
 submitbutton.addEventListener('click',function(){
@@ -19,7 +20,10 @@ submitbutton.addEventListener('click',function(){
         var uvIndexValue = data['weather'][0]['description'];
 
         var dateValue = Date(data['dt']);
-
+        // Displays Icon
+        var icon = data.weather[0].icon;
+        var imageValue = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        console.log(imageValue);
         // var date = dateValue.getDate();
         // var month = dateValue.getMonth(); //Be careful! January is 0 not 1
         // var year = dateValue.getFullYear();
@@ -31,6 +35,7 @@ submitbutton.addEventListener('click',function(){
         wind.innerHTML = windValue + ' MPH';
         humi.innerHTML = humiValue +'%';
         uvIndex.innerHTML = uvIndexValue;
+        weatherIcon.innerHTML = '<img src='+imageValue+'></img>';
         // let lonValue = data['coord']['lon'];
         // let latValue = data['coord']['lat'];
 
@@ -40,28 +45,42 @@ submitbutton.addEventListener('click',function(){
 
 // Add event for city buttons
 // define a function
-var getCity = function(event) {
+// var getCity = function(event) {
     
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+event.target.value+'&appid=6e61be1535f6225a901edf5250e2ae2f')
-    .then(response => response.json())
-    .then(data => {
-        var cityValue = data['name'];
-        var tempValue = data['main']['temp'];
-        var descValue = data['weather'][0]['description'];
-        city.innerHTML = cityValue;
-        temp.innerHTML = tempValue;
-        desc.innerHTML = descValue;
+//     fetch('https://api.openweathermap.org/data/2.5/weather?q='+event.target.value+'&appid=6e61be1535f6225a901edf5250e2ae2f')
+//     .then(response => response.json())
+//     .then(data => {
+//         var cityValue = data['name'];
+//         var tempValue = data['main']['temp'];
+//         var descValue = data['weather'][0]['description'];
+//         city.innerHTML = cityValue;
+//         temp.innerHTML = tempValue;
+//         desc.innerHTML = descValue;
 
-    })
-.catch(err => alert("Wrong city name!"))
-}
+//     })
+// .catch(err => alert("Wrong city name!"))
+// }
 
-var bigcitybuttons = document.getElementsByClassName('button');
-for (let i=0; i<bigcitybuttons.length; i++) {
-    var bigcitybutton = bigcitybuttons[i];
+// var bigcitybuttons = document.getElementsByClassName('button');
+// for (let i=0; i<bigcitybuttons.length; i++) {
+//     var bigcitybutton = bigcitybuttons[i];
     
-    bigcitybutton.addEventListener('click', getCity);
+//     bigcitybutton.addEventListener('click', getCity);
     
-}
+// }
 
+// {   "coord": {"lon":-87.65,"lat":41.85},
+//     "weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04n"}],
+//     "base":"stations",
+//     "main":{"temp":286.99,"feels_like":286.54,"temp_min":285.39,"temp_max":288.31,"pressure":1019,"humidity":81},
+//     "visibility":10000,
+//     "wind":{"speed":2.58,"deg":14,"gust":3.79},
+//     "clouds":{"all":100},
+//     "dt":1653183890,
+//     "sys":{"type":2,"id":2005153,"country":"US","sunrise":1653128713,"sunset":1653181768},
+//     "timezone":-18000,
+//     "id":4887398,
+//     "name":"Chicago",
+//     "cod":200
+// }
 
--- README.md
+++ README.md
@@ -7,6 +7,12 @@ We are designing a weather dashboard to help travelers who want to see the weath
 - Add the search button
 - Add the date for the current weather
 - Add city name, humidity, temperature, etc to the dashboard
+- TODO: Add an icon  that represent the weather condition
+- Add a color that indicates whether the conditions are favorable, moderate, or severe
+- Add future weather condition for that city
+- Add 5-day forecast with dates, icons, temperatures, wind speeds and humidities
+- Display search history
+- Add the function to navigate to the info related to the city in search history
 
 ## Screenshot
 
